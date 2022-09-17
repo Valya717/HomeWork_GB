56d82136109b9f4bc179212d8bd1cd8d8932f086
Valentina <valyak1988@mail.ru>
2022-09-17 11:10:54 +0500

    Задача 2 решение

--- c#_02/Program.cs
+++ c#_02/Program.cs
@@ -2,3 +2,27 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число: ");
+int num;
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int num2;
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num > num2)
+    {
+        Console.Write("Большее число: ");
+    Console.WriteLine(num);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(num2);
+    }
+
+else
+    {
+   Console.Write("Большее число: ");
+    Console.WriteLine(num2);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(num);
+    }
