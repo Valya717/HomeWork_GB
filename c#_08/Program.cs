bb85bc2bcbe1fbf966d05d924b2462dc74331b77
Valentina <valyak1988@mail.ru>
2022-09-17 20:00:31 +0500

    Задача 8 решение

--- c#_08/Program.cs
+++ c#_08/Program.cs
@@ -2,18 +2,23 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.Write("Введите число больше 1: ");
+Console.Write("Введите число: ");
 int num;
 num = Convert.ToInt32(Console.ReadLine());
 int res = 2;
-
-while (num >= res)
+if (num ==1)
     {
-    Console.Write(res + ", ");
-    res+=2;
+    Console.WriteLine(" Нет четных чисел от 1 до 1 " );
     }
+else
+    {while (num >= res)
+        {
+        Console.Write(res + ", ");
+        res+=2;
+        }
 
-Console.Write(" - четные числа от 1 до " );
-Console.WriteLine(num);
+    Console.Write(" - четные числа от 1 до " );
+    Console.WriteLine(num);
+    }
 
 
