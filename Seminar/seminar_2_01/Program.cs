3efec42fc04be157fc038bb416764af048993b2e
Valentina <valyak1988@mail.ru>
2022-09-26 13:16:12 +0500

    Объединение папок

---
+++ Seminar/seminar_2_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и
+ // показывает наибольшую цифру числа. 
+ //78 -> 8 
+ // 12-> 2 
+ // 85 -> 8
+
+ 
+ int n = new Random().Next (10,100); //[10...99)
+ Console.WriteLine (n);
+ int x = n/10;
+ int r = n - x*10;
+ if (x > r)
+Console.WriteLine (x);
+ else Console.WriteLine (r);
