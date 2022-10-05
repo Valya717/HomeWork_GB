99dac8c95368ed565e4e5806877b16749e90b736
Valentina <valyak1988@mail.ru>
2022-10-05 15:00:39 +0500

    функции и массивы

--- Lection/Example008_Intro/Program.cs
+++ Lection/Example008_Intro/Program.cs
@@ -5,7 +5,7 @@
     if(arg3>result) result = arg3;
     return result;
 }
-int a1 = 1;
+int a1 = 125;
 int b1 = 2;
 int c1 = 3;
 
@@ -17,10 +17,14 @@ int a3 = 7;
 int b3 = 8;
 int c3 = 9;
 
-int max1 = Max(a1, b1, c1);
+/*int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);*/
 
-int max = Max(max1, max2, max3);
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
