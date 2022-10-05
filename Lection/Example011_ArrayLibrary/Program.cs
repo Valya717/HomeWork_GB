1751ca70497738656c413d96f54fe976ec382a7e
Valentina <valyak1988@mail.ru>
2022-10-05 16:29:55 +0500

    методы массивы

--- Lection/Example011_ArrayLibrary/Program.cs
+++ Lection/Example011_ArrayLibrary/Program.cs
@@ -1,4 +1,49 @@
-﻿int[] array = new int[10];
+﻿void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,5);
+        index ++;
+    }
+}
+
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine( col[position]);
+        position ++;
+    }
+}
+
+int IndexOf(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index ++;
+    }
+    return position;
+}
+
+int [] array = new int [10];
+
 FillArray(array);
 PrintArray(array);
 
+Console.WriteLine();
+
+int pos = IndexOf (array, 1);
+Console.WriteLine(pos);
