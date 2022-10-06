989dec581e1598f38c1d9d960c6742f418cd9d34
Valentina <valyak1988@mail.ru>
2022-10-06 12:15:52 +0500

    task 21

---
+++ HomeWork/с#_21/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+double [] coord1 = new double [3];
+int i = 0;
+Console.Write("Первая точка имеет координаты (X,Y,Z): ");
+while (i<coord1.Length )
+{
+    coord1[i] = new  Random().Next(-50,51);
+    Console.Write(coord1[i] + " ");
+    i++;
+}
+double [] coord2 = new double [3];
+i = 0;
+Console.Write("\nВторая точка имеет координаты (X,Y,Z): ");
+while (i<coord2.Length )
+{
+    coord2[i] = new  Random().Next(-50,51);
+    Console.Write(coord2[i] + " ");
+    i++;
+}
+Console.WriteLine();
+
+
+i = 0;
+double sum =0;
+while (i<coord1.Length)
+{
+    double x = Math.Abs (coord1[i] - coord2[i]);
+    sum = sum + x*x;
+    i++;
+}
+double result = Math.Sqrt (sum);
+Console.WriteLine ("Расстояние между первой и второй точкой: " + result);
