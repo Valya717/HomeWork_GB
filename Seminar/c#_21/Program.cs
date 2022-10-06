f24b73798e988325e971c7de0344438e7145c0ef
Valentina <valyak1988@mail.ru>
2022-10-06 10:58:08 +0500

    task 21

--- Seminar/c#_21/Program.cs
+++ Seminar/c#_21/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int [] coord1 = new int [2];
+int i = 0;
+Console.Write("Первая точка имеет координаты (X,Y): ");
+while (i<coord1.Length )
+{
+    coord1[i] = new Random().Next(-50,51);
+    Console.Write(coord1[i] + " ");
+    i++;
+}
+int [] coord2 = new int [2];
+i = 0;
+Console.Write("\nВторая точка имеет координаты (X,Y): ");
+while (i<coord2.Length )
+{
+    coord2[i] = new Random().Next(-50,51);
+    Console.Write(coord2[i] + " ");
+    i++;
+}
+Console.WriteLine();
+
+i = 0;
+int sum =0;
+while (i<coord1.Length)
+{
+    int x = Math.Abs (coord1[i] - coord2[i]);
+    sum = sum + x*x;
+    i++;
+}
+double result = Math.Sqrt (sum);
+Console.WriteLine ("Расстояние между первой и второй точкой: " + result);
