ed1007df215c3ac2b4ce5d782b19cafe1b5d7796
Valentina <valyak1988@mail.ru>
2022-10-07 14:36:29 +0500

    task 30

---
+++ Seminar/c#_30/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из воьми элеметов , заполненный 0 и 1 в случайном порядке
+
+Console.Clear();
+
+int [] array = new int [8];
+int i = 0;
+while (i<array.Length)
+{
+    array[i] = new Random().Next(0,2);
+    Console.Write(array[i] +" ");
+    i++;
+}
+Console.WriteLine();
